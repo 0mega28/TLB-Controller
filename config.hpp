591dff4e125b2535c7758f01c531962273a9e948
Abhay Kumar <abhay98394@gmail.com>
2022-02-05 22:53:10 +0530

    Add include guards to header file
    Co-authored-by: Saksham Gupta <sakshamdam54321@gmail.com>
    Co-authored-by: Abhay Kumar <abhay98394@gmail.com>

--- config.hpp
+++ config.hpp
@@ -2,3 +2,4 @@
 
 unsigned int bits_in_addressing = 32;
 unsigned int page_size = 4096; // 4KB
+std::size_t tlb_size = 30;
--- pageTable.hpp
+++ pageTable.hpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <vector>
 #include <stdint.h>
 #include "config.hpp"
