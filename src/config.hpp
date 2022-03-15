87a61f91fb44e6585a9dcd3ee552e2435027e565
Saksham Gupta <sakshamdam54321@gmail.com>
2022-03-16 00:55:52 +0530

    Silly mistake ;-)

--- src/config.hpp
+++ src/config.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#define BITS_IN_ADDRESSING = 32
-#define PAGE_SIZE = 4096 // 4KB
-#define TLB_SIZE = 256
-#define NUM_OF_WAYS = 4 // k-way set associative mapping
+#define BITS_IN_ADDRESSING 32
+#define PAGE_SIZE 4096 // 4KB
+#define TLB_SIZE 256
+#define NUM_OF_WAYS 4 // k-way set associative mapping
