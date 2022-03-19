02c8b81f001a86cb0437380196c2b3ba375e1bc3
Shourya Shukla <periperidip@gmail.com>
2022-03-19 15:38:57 +0530

    main: assume PA is equal to VA
    Since the job of a TLB is to deliver a physical frame number
    corresponding to a virtual page number, assume that the physical address
    is equal to the virtual address irrespective of the instruction type,
    i.e., read or write.
    
    This assumption is made for the simplicity of the simulation and is in
    fact technically inaccurate.
    
    Signed-off-by: Abhay Kumar <abhay98394@gmail.com>
    Signed-off-by: Shourya Shukla <periperidip@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -33,34 +33,28 @@ void readInputTraces(const char *file,
 		std::string pa;
 
 		ss >> type;
-		if (type == pt_entry)
+		if (type == pt_entry || type == va_entry)
 		{
-			// 2246 R 0x3ad97780 0x114ac
 			ss >> va;
-			ss >> pa;
 
 			/* Drop 0x from start */
 			va = va.substr(2);
-			pa = pa.substr(2);
+			/*
+			 * For every request irrespective of a read or
+			 * a write, the job of the TLB is to simply
+			 * deliver a physical frame number mapped to a
+			 * virtual page number. Therefore, for the
+			 * sake of the simulation, the PA is assumed
+			 * to be equal to the VA.
+			 */
+			pa = va;
 
 			uint64_t va_num = std::stoul(va, nullptr, 16);
 			uint64_t pa_num = std::stoul(pa, nullptr, 16);
 
 			page_table_vector.push_back(std::make_pair(va_num, pa_num));
-		}
-		else if (type == va_entry)
-		{
-			// 3 W 0x36167780
-			ss >> va;
-
-			/* Drop 0x from start */
-			va = va.substr(2);
-
-			uint64_t va_num = std::stoul(va, nullptr, 16);
 			test_VA_Vector.push_back(va_num);
-		}
-		else
-		{
+		} else {
 			std::cerr << "Error: Invalid trace entry" << std::endl;
 			exit(EXIT_FAILURE);
 		}
@@ -74,19 +68,18 @@ int main(int argc, char **argv)
 		std::cerr << "Usage: " << argv[0] << " <trace file>"  << std::endl;
 		exit(1);
 	}
+
 	/*
 	 * USIMM traces file have two types of lines
 	 * 1) 2 R 0x3a617780 0x114c8
 	 * 2) 3 W 0x36167780
 	 *
-	 * For R instruction store treat 3rd and 4th column
-	 * as VA and PA respectively and store them in page_table_vector
-	 *
-	 * For W instruction store 3rd column as VA
-	 * and use the address to find the corresponding PA using TLB
+	 * For both the instruction types, treat the 3rd column
+	 * as the VA and assume PA is equal to the VA, storing them in
+	 * the 'page_table_vector'.
 	 */
-
 	const char *inputfile = argv[1];
+
 	// TODO: unhardcode this and make output file name dynamic from input file
 	const char *outputfile = "output.txt";
 
