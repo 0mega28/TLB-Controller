96e59b5f4105b67b2569ce96061d14def515c4dc
Shourya Shukla <periperidip@gmail.com>
2022-03-21 02:17:43 +0530

    TLB: rename 'set_block()' to 'insert_block()' for clarity
    Rename the function 'set_block()' to 'insert_block()' for clarity. While
    at it, amend the usage of 'Set::insert_block()' as well.
    
    Signed-off-by: Abhay Kumar <abhay98394@gmail.com>
    Signed-off-by: Shourya Shukla <periperidip@gmail.com>

--- src/TLB.hpp
+++ src/TLB.hpp
@@ -18,12 +18,13 @@ public:
 	/* Returns frame_number if block found else BLOCK_NOT_FOUND */
 	uint64_t get_frame_number(uint64_t page_number);
 
-	/* 
-	 * Sets a block by following LRU replacement policy 
-	 * Returns the evicted block so that it can be pushed to next level of cache
-	 * If no eviction is needed, it returns a block with last accessed time of BLOCK_NOT_ACCESSED
+	/*
+	 * Inserts a block by evicting a block using the LRU replacement
+	 * policy. Returns the evicted block so that it can be pushed to
+	 * next level of cache. If no eviction is needed, it returns a
+	 * block with last accessed time of BLOCK_NOT_ACCESSED.
 	 */
-	Block set_block(uint64_t page_number, uint64_t frame_number);
+	Block insert_block(uint64_t page_number, uint64_t frame_number);
 };
 
 TLB::TLB(unsigned int tlb_size, unsigned int num_ways)
@@ -56,8 +57,8 @@ uint64_t TLB::get_frame_number(uint64_t page_number)
 	return this->sets[index]->get_frame_number(page_number);
 }
 
-Block TLB::set_block(uint64_t page_number, uint64_t frame_number)
+Block TLB::insert_block(uint64_t page_number, uint64_t frame_number)
 {
 	unsigned int index = this->get_index_block(page_number);
-	return this->sets[index]->set_block(page_number, frame_number);
+	return this->sets[index]->insert_block(page_number, frame_number);
 }
