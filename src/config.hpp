7b7d47e10b597f729d69bd475416269f4ec5d6fd
Saksham Gupta <sakshamdam54321@gmail.com>
2022-03-21 14:43:06 +0530

    config: set TLB_SIZE 1024 and NUM_OF_WAYS 8
    hit rate increased to 92 from 78

--- src/config.hpp
+++ src/config.hpp
@@ -2,5 +2,5 @@
 
 #define BITS_IN_ADDRESSING 32
 #define PAGE_SIZE 4096 // 4KB
-#define TLB_SIZE 256
-#define NUM_OF_WAYS 4 // k-way set associative mapping
+#define TLB_SIZE 1024
+#define NUM_OF_WAYS 8 // k-way set associative mapping
