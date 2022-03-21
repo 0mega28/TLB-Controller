a048b548a9105d523b2703ddc42ac59b3c74dafb
Saksham Gupta <sakshamdam54321@gmail.com>
2022-03-21 16:54:36 +0530

    runall.sh: Add run shell script
    And move scripts to scripts directory

---
+++ src/scripts/runall.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# cd to src directory
+cd "$(dirname "$0")/.." || exit
+
+input_dir="input"
+output_dir="output"
+program="./tlbcontroller"
+outputfile="tlbstatistics.txt"
+
+# Create output directory if it does not exist
+mkdir -p "${output_dir}"
+
+# Get all input test cases
+input_files=$(find "${input_dir}" -type f)
+
+# Compile tlbcontroller
+make --always-make
+
+# Clear output file
+rm -f "${outputfile}"
+
+for files in $input_files; do
+	(
+		echo "Running test case: ${files}"
+
+		{
+			"${program}" "${files}"
+			echo "Test case: ${files}"
+			echo "-------------------------------------------------------"
+		} >> "${outputfile}"
+
+		echo "Completed test case: ${files}"
+	) &
+done
+
+wait
